edfde162db1338f0e0d6d3c763be0fb504bc9764
danibhern <dan.barrerah@duocuc.cl>
2026-06-07 12:35:52 -0400

    Inicio de  donaton-ms-necesidades

---
+++ src/test/java/com/example/ms_necesidades/MsNecesidadesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ms_necesidades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsNecesidadesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
